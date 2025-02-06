0d7ab62a261538d66a3c0ebdec024caee283e882
Andrei Rudchenko <andrei.rudchenko.qubership@gmail.com>
2025-02-06 11:11:43 +0300

    QBRSHP-0000. Added new script and fixed the dockerfile

---
+++ build_gcip/scripts/github_actions.py
@@ -0,0 +1,32 @@
+import os
+from os import *
+from pprint import pformat
+from dataclasses import fields
+
+import click
+
+from envgenehelper import logger
+from gitlab_ci import build_pipeline
+from validations import validate_pipeline
+from pipeline_parameters import PipelineParameters
+
+@click.group()
+def cli():
+    pass
+
+def prepare_input_params() -> PipelineParameters:
+    params = PipelineParameters()
+    params_log = "Input parameters are:"
+    for param in fields(params):
+        params_log += f"\n{param.name.upper()}: {pformat(getattr(params, param.name))}"
+    logger.info(params_log)
+    return params
+
+
+@cli.command("validate_pipeline")
+def validate_pipeline_command():
+    params = prepare_input_params()
+    validate_pipeline(params)
+
+if __name__ == "__main__":
+ cli()
