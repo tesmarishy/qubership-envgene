dcf0b161c0581ed28bbbaf14686e8cea17b38132
Andrei Rudchenko <andrei.rudchenko.qubership@gmail.com>
2025-02-10 12:02:04 +0300

    QBRSHP-0000.Added the logging

--- scripts/build_env/env_inventory_generation.py
+++ scripts/build_env/env_inventory_generation.py
@@ -32,14 +32,25 @@ class Environment:
 
     def __post_init__(self):
         self.env_path = path.join(self.base_dir, "environments", self.cluster, self.name)
+        print(f"env_path: {self.env_path}")
 
         self.inventory_path = helper.getEnvDefinitionPath(self.env_path)
+        print(f"inventory_path: {self.inventory_path}")
+
         self.creds_path = helper.getEnvCredentialsPath(self.env_path)
+        print(f"creds_path: {self.creds_path}")
+
         self.inv_gen_creds_path = path.join(self.env_path, INV_GEN_CREDS_PATH)
+        print(f"inv_gen_creds_path: {self.inv_gen_creds_path}")
 
         self.inventory = helper.openYaml(self.inventory_path, allow_default=True)
+        print(f"inventory: {self.inventory}")
+
         self.creds = helper.decrypt_file(self.creds_path, in_place=False, allow_default=True)
+        print(f"creds: {self.creds}")
+
         self.inv_gen_creds = helper.decrypt_file(self.inv_gen_creds_path, in_place=False, allow_default=True)
+        print(f"inv_gen_creds: {self.inv_gen_creds}")
 
 def generate_env():
     base_dir = getenv_and_log('CI_PROJECT_DIR')
