dbaa41335fe08355de2c05df1d98e98fe14dd02c
Andrei Rudchenko <andrei.rudchenko.qubership@gmail.com>
2025-05-26 10:29:55 +0300

    Adding valid tests

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-diff_status=0
-git diff --cached --exit-code || diff_status=$?
-if [ $diff_status -ne 0 ]; then 
-  echo "See diff above for changed files. Committing..."
-else
-  echo "We have NOTHING to commit. Skipping..."
-fi
