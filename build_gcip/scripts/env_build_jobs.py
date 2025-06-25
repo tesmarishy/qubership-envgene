4fd3b41d3d95ed5c0099375aea19d69493962b50
Tesma Jose <tesma.jose@netcracker.com>
2025-06-25 16:45:48 +0530

    fixed the export issue

--- build_gcip/scripts/env_build_jobs.py
+++ build_gcip/scripts/env_build_jobs.py
@@ -108,8 +108,6 @@ def prepare_git_commit_job(pipeline, full_env, enviroment_name, cluster_name, de
       "image":  '${envgen_image}',
       "stage":  'git_commit',
       "script": [ 'if [ -d "${CI_PROJECT_DIR}/configuration/certs" ]; then cert_path=$(ls -A "${CI_PROJECT_DIR}/configuration/certs"); for path in $cert_path; do . /module/scripts/update_ca_cert.sh ${CI_PROJECT_DIR}/configuration/certs/$path; done; fi',
-                 "echo testing"
-                  "export DEPLOYMENT_SESSION_ID=${deployment_session_id}",
                   f'/module/scripts/prepare.sh "git_commit.yaml"',
                   "export env_name=$(echo $ENV_NAME | awk -F '/' '{print $NF}')",
                   'env_path=$(sudo find $CI_PROJECT_DIR/environments -type d -name "$env_name")',
@@ -131,7 +129,7 @@ def prepare_git_commit_job(pipeline, full_env, enviroment_name, cluster_name, de
       "module_config_default": "/module/templates/defaults.yaml",
       "GIT_STRATEGY": "none",
       "COMMIT_ENV": "true",
-      "DEPLOYMENT-SESSION-ID": deployment_session_id
+      "DEPLOYMENT_SESSION_ID": deployment_session_id
   }
   git_commit_job = job_instance(params=git_commit_params, vars=git_commit_vars)
   git_commit_job.artifacts.add_paths("${CI_PROJECT_DIR}/environments/" + f"{full_env}")
