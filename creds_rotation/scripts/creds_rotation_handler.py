d182166305bb0e9adb9721eacbc7035989200fb1
Tesma Jose <tesma.clara@gmail.com>
2025-07-17 15:55:34 +0530

    changes to check if it has taken affect

--- creds_rotation/scripts/creds_rotation_handler.py
+++ creds_rotation/scripts/creds_rotation_handler.py
@@ -16,16 +16,16 @@ import envgenehelper.logger as logger
 
 def print_total_memory():
     total_mem = psutil.virtual_memory().total / (1024 ** 2)
-    print(f"🧠 Total system memory: {total_mem:.2f} MB")
+    print(f"Total system memory: {total_mem:.2f} MB")
 
 def print_deep_map_size(name: str, map_obj: dict):
     size_in_mb = asizeof.asizeof(map_obj) / (1024 * 1024)
-    print(f"📊 Map '{name}' deep size: {size_in_mb:.2f} MB")
+    print(f"Map '{name}' deep size: {size_in_mb:.2f} MB")
 
 def print_memory_usage():
     process = psutil.Process(os.getpid())
     mem_in_mb = process.memory_info().rss / (1024 * 1024)
-    print(f"🔍 Current memory usage: {mem_in_mb:.2f} MB")
+    print(f" Current memory usage: {mem_in_mb:.2f} MB")
 
 def load_payload(payload: str):
     if isinstance(payload, str):
