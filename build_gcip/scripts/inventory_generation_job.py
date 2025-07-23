da1c21b0b332c119d576407bebb47f5ca51fa79a
basudev91 <basudev.sahoo@netcracker.com>
2025-07-23 11:19:24 +0530

    Update inventory_generation_job.py (#409)
    Co-authored-by: Jackson Raj A <150916845+BackendBits@users.noreply.github.com>

--- build_gcip/scripts/inventory_generation_job.py
+++ build_gcip/scripts/inventory_generation_job.py
@@ -11,7 +11,8 @@ def is_inventory_generation_needed(is_template_test, inv_gen_params):
         return True
     params_processed_by_inv_gen = ['ENV_SPECIFIC_PARAMETERS', 'ENV_TEMPLATE_NAME', 'SD_DATA', 'SD_VERSION']
     for param in params_processed_by_inv_gen:
-        if inv_gen_params[param] != '':
+        #if inv_gen_params[param] != '':
+        if inv_gen_params[param] not in (None, ''):
             return True
     return False
 
