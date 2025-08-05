d79884bde60172e9833567e30ac957cc41e29648
Tesma Jose <tesma.jose@netcracker.com>
2025-08-05 15:58:30 +0530

    adding tests in github actions for java module

---
+++ build_effective_set_generator_java/effective-set-generator/src/test/java/org/qubership/cloud/devops/cli/TestConstants.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024-2025 NetCracker Technology Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.qubership.cloud.devops.cli;
+
+public class TestConstants {
+
+    public static final String ENV_ID = "c01/e01";
+    public static final String EFFECTIVE_SET_VERSION = "v1.0";
+    public static final String ENV_FOLDER = "src/test/resources/environments";
+    public static final String SBOM_PATH = "src/test/resources/environments/c01/e01/Inventory/solution-descriptor/sboms";
+    public static final String SOL_SBOM = "src/test/resources/environments/c01/e01/Inventory/solution-descriptor/solution-sbom.json";
+    public static final String REG_PATH = "src/test/resources/environments/c01/e01/configurations/registry.yml";
+    public static final String STORED_CREDENTIALS_YAML = "test-data/ns-test.application-test/credentials.yaml";
+    public static final String STORED_PARAMS_YAML = "test-data/ns-test.application-test/deployment-parameters.yaml";
+    public static final String GENERATED_CREDENTIALS_YAML = "ns-test/application-test/credentials.yaml";
+    public static final String GENERATED_PARAMS_YAML = "ns-test/application-test/deployment-parameters.yaml";
+    public static final String STORED_TECHPARAM_YAML = "test-data/ns-test.application-test/technical-configuration-parameters.yaml";
+    public static final String GENERATED_TECHPARAM_YAML = "ns-test/application-test/technical-configuration-parameters.yaml";
+    public static final String STORED_MAPPING_YAML = "test-data/mapping.yaml";
+    public static final String GENERATED_MAPPING_YAML = "mapping.yaml";
+
+    //Effective Param Set 2
+    public static final String TEST_DATA_ES_2_RUNTIME_NS_1_APP_NAME_0_PARAMETERS_YAML = "test-data/es2/runtime/ns-1/app-name-0/parameters.yaml";
+    public static final String RUNTIME_NS_1_APP_NAME_0_PARAMETERS_YAML = "runtime/ns-1/app-name-0/parameters.yaml";
+    public static final String RUNTIME_NS_1_APP_NAME_0_CREDENTIALS_YAML = "runtime/ns-1/app-name-0/credentials.yaml";
+    public static final String TEST_DATA_ES_2_RUNTIME_NS_1_APP_NAME_0_CREDENTIALS_YAML = "test-data/es2/runtime/ns-1/app-name-0/credentials.yaml";
+    public static final String DEPLOYMENT_MAPPING_YAML = "deployment/mapping.yaml";
+    public static final String TEST_DATA_ES_2_DEPLOYMENT_MAPPING_YAML = "test-data/es2/deployment/mapping.yaml";
+    public static final String PIPELINE_CONSUMER_02_PARAMETERS_YAML = "pipeline/consumer-02-parameters.yaml";
+    public static final String TEST_DATA_ES_2_PIPELINE_CONSUMER_02_PARAMETERS_YAML = "test-data/es2/pipeline/consumer-02-parameters.yaml";
+    public static final String PIPELINE_CONSUMER_01_PARAMETERS_YAML = "pipeline/consumer-01-parameters.yaml";
+    public static final String TEST_DATA_ES_2_PIPELINE_CONSUMER_01_PARAMETERS_YAML = "test-data/es2/pipeline/consumer-01-parameters.yaml";
+    public static final String PIPELINE_CREDENTIALS_YAML = "pipeline/credentials.yaml";
+    public static final String TEST_DATA_ES_2_PIPELINE_CREDENTIALS_YAML = "test-data/es2/pipeline/credentials.yaml";
+    public static final String PIPELINE_PARAMETERS_YAML = "pipeline/parameters.yaml";
+    public static final String TEST_DATA_ES_2_PIPELINE_PARAMETERS_YAML = "test-data/es2/pipeline/parameters.yaml";
+    public static final String DEPLOYMENT_NS_1_APP_NAME_0_VALUES_DEPLOYMENT_PARAMETERS_YAML = "deployment/ns-1/app-name-0/values/deployment-parameters.yaml";
+    public static final String TEST_DATA_ES_2_DEPLOYMENT_NS_1_APP_NAME_0_VALUES_DEPLOYMENT_PARAMETERS_YAML = "test-data/es2/deployment/ns-1/app-name-0/values/deployment-parameters.yaml";
+    public static final String DEPLOYMENT_NS_1_APP_NAME_0_VALUES_CREDENTIALS_YAML = "deployment/ns-1/app-name-0/values/credentials.yaml";
+    public static final String TEST_DATA_ES_2_DEPLOYMENT_NS_1_APP_NAME_0_VALUES_CREDENTIALS_YAML = "test-data/es2/deployment/ns-1/app-name-0/values/credentials.yaml";
+    public static final String SRC_TEST_RESOURCES_ENVIRONMENTS_C_01_E_02_INVENTORY_SOLUTION_DESCRIPTOR_SBOMS = "src/test/resources/environments/c01/e02/Inventory/solution-descriptor/sboms";
+    public static final String SRC_TEST_RESOURCES_ENVIRONMENTS_C_01_E_02_INVENTORY_SOLUTION_DESCRIPTOR_SOLUTION_SBOM_JSON = "src/test/resources/environments/c01/e02/Inventory/solution-descriptor/solution-sbom.json";
+    public static final String SRC_TEST_RESOURCES_ENVIRONMENTS_C_01_E_02_CONFIGURATIONS_REGISTRY_YML = "src/test/resources/environments/c01/e02/configurations/registry.yml";
+    public static final String V_2_0 = "v2.0";
+    public static final String SRC_TEST_RESOURCES_ENVIRONMENTS_C_01_E_02_CONSUMER_CONSUMER_01_JSON = "src/test/resources/environments/c01/e02/consumer/consumer-01.json";
+    public static final String SRC_TEST_RESOURCES_ENVIRONMENTS_C_01_E_02_CONSUMER_CONSUMER_02_JSON = "src/test/resources/environments/c01/e02/consumer/consumer-02.json";
+    public static final String C_01_E_02 = "c01/e02";
+}
