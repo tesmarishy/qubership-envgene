dc71096c100e18bf1ec6b41b024e9468b29642f4
Nurlybek Kamelov <79522742+GlimmerCape@users.noreply.github.com>
2025-08-11 16:20:55 +0500

    feat: update build_modules.sh for local testing (#507)

--- python/build_modules.sh
+++ python/build_modules.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -e
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo "Installing $SCRIPTPATH/envgene"
-pip install $SCRIPTPATH/envgene
-echo "Removing build trash..."
-rm -r $SCRIPTPATH/envgene/build
-rm -r $SCRIPTPATH/envgene/envgenehelper.egg-info
-echo "Installing $SCRIPTPATH/jschon-sort"
-pip install $SCRIPTPATH/jschon-sort
-echo "Removing build trash..."
-rm -r $SCRIPTPATH/jschon-sort/build
-rm -r $SCRIPTPATH/jschon-sort/jschon_sort.egg-info
-echo "Installing $SCRIPTPATH/integration"
-pip install $SCRIPTPATH/integration
-echo "Removing build trash..."
-rm -r $SCRIPTPATH/integration/build
-rm -r $SCRIPTPATH/integration/integration_loader.egg-info
+SCRIPTPATH="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"
+
+install_and_clean() {
+    local path="$1"
+    local name="$2"
+    local base_path="${3:-$SCRIPTPATH}"  # optional third argument to function
+
+    echo "Installing $base_path/$path"
+    if [ "$IS_LOCAL_DEV_TEST_ENVGENE" = "true" ]; then
+      uv pip install --system -e "$base_path/$path"
+    else
+      uv pip install --system "$base_path/$path"
+    fi
+
+    echo "Removing build trash..."
+    rm -rf "$base_path/$path/build" "$base_path/$path/$name.egg-info"
+}
+
+pip install uv # pip replacer, makes this script run ~2.8x faster
+install_and_clean "envgene" "envgenehelper"
+install_and_clean "jschon-sort" "jschon_sort"
+install_and_clean "integration" "integration_loader"
+
